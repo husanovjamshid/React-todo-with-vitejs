48467aa436f9e52f6f3b3927362bc9027a49c4a4
husanovjamshid <husanovjamshid11@gmail.com>
2023-02-06 13:27:06 +0500

    change style

--- src/components/Registration/Login/Login.jsx
+++ src/components/Registration/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useContext, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { authContext } from "../../context/AuthContext";
-import './login.scss'
+import "./login.scss";
 export const Login = () => {
   let { setToken } = useContext(authContext);
 
@@ -36,43 +36,48 @@ export const Login = () => {
         height: "100vh",
       }}
     >
-      <form onSubmit={handleSubmit} className="shadow-lg register__form shadow-lg rounded-3 pt-3 p-5">
-        <h2 className="text-center mb-3">Login</h2>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <div className="container">
+        <form
+          onSubmit={handleSubmit}
+          className="shadow-lg register__form shadow-lg rounded-3 pt-3 p-5"
+        >
+          <h2 className="text-center mb-3">Login</h2>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
 
       {/* <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }} /> */}
     </div>
--- src/components/Registration/Register/Register.jsx
+++ src/components/Registration/Register/Register.jsx
@@ -8,7 +8,7 @@ export const Register = () => {
   let lastRef = useRef();
   let emailRef = useRef();
   let passwordRef = useRef();
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -23,7 +23,7 @@ export const Register = () => {
       .then((data) => {
         if (data.status === 201) {
           setToken(data.data.accessToken);
-          navigate('/')
+          navigate("/");
         }
       })
       .catch((err) => console.log(err));
@@ -39,68 +39,70 @@ export const Register = () => {
         height: "100vh",
       }}
     >
-      <form
-        onSubmit={handleSubmit}
-        className="shadow register shadow-lg rounded-3 pt-3 p-5"
-      >
-        <h2 className="text-center mb-3">Register</h2>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail11" className="form-label">
-            First name
-          </label>
-          <input
-            ref={nameRef}
-            type="text"
-            className="form-control"
-            id="exampleInputEmail11"
-            aria-describedby="emailHelp"
-          />
-          <label htmlFor="exampleInputEmail31" className="form-label">
-            Last name
-          </label>
-          <input
-            ref={lastRef}
-            type="text"
-            className="form-control"
-            id="exampleInputEmail31"
-            aria-describedby="emailHelp"
-          />
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            ref={emailRef}
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Password
-          </label>
-          <input
-            ref={passwordRef}
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      <div className="container">
+        <form
+          onSubmit={handleSubmit}
+          className="shadow register shadow-lg rounded-3 pt-3 p-5"
+        >
+          <h2 className="text-center mb-3">Register</h2>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail11" className="form-label">
+              First name
+            </label>
+            <input
+              ref={nameRef}
+              type="text"
+              className="form-control"
+              id="exampleInputEmail11"
+              aria-describedby="emailHelp"
+            />
+            <label htmlFor="exampleInputEmail31" className="form-label">
+              Last name
+            </label>
+            <input
+              ref={lastRef}
+              type="text"
+              className="form-control"
+              id="exampleInputEmail31"
+              aria-describedby="emailHelp"
+            />
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              ref={emailRef}
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              ref={passwordRef}
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Check me out
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
 
       {/* <div className="mask" style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }} /> */}
     </main>
